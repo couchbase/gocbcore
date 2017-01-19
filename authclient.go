f4f462851e88a14550c3c09c69af13a2f63c4c60
Brett Lawson <brett19@gmail.com>
2017-01-19 15:46:02 -0800

    GOCBC-101: Implement SCRAM-SHA authentication methods.
    Change-Id: I546e7049a13bad8f101307d18433807d5ec035cb
    Reviewed-on: http://review.couchbase.org/72253
    Reviewed-by: Matt Ingenthron <matt@couchbase.com>
    Reviewed-by: Brett Lawson <brett19@gmail.com>
    Tested-by: Brett Lawson <brett19@gmail.com>

--- authclient.go
+++ authclient.go
@@ -1,6 +1,10 @@
 package gocbcore
 
 import (
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"hash"
 	"time"
 )
 
@@ -34,6 +38,55 @@ func SaslAuthPlain(username, password string, client AuthClient, deadline time.T
 	return err
 }
 
+func saslAuthScram(saslName []byte, newHash func() hash.Hash, username, password string, client AuthClient, deadline time.Time) error {
+	scramMgr := newScramClient(newHash, username, password)
+
+	var in []byte
+	var err error
+
+	// Perform the initial SASL step
+	scramMgr.Step(nil)
+	in, err = client.ExecSaslAuth(saslName, scramMgr.Out(), deadline)
+	if err != nil && err != ErrAuthContinue {
+		return err
+	}
+
+	// Perform any additional step rounds
+	for err == ErrAuthContinue {
+		if !scramMgr.Step(in) {
+			err = scramMgr.Err()
+			if err != nil {
+				return err
+			}
+
+			logErrorf("Local auth client finished before server accepted auth")
+			return ErrAuthError
+		}
+
+		in, err = client.ExecSaslStep(saslName, scramMgr.Out(), deadline)
+		if err != nil && err != ErrAuthContinue {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// SaslAuthScramSha1 performs SCRAM-SHA1 SASL authentication against an AuthClient.
+func SaslAuthScramSha1(username, password string, client AuthClient, deadline time.Time) error {
+	return saslAuthScram([]byte("SCRAM-SHA1"), sha1.New, username, password, client, deadline)
+}
+
+// SaslAuthScramSha256 performs SCRAM-SHA256 SASL authentication against an AuthClient.
+func SaslAuthScramSha256(username, password string, client AuthClient, deadline time.Time) error {
+	return saslAuthScram([]byte("SCRAM-SHA256"), sha256.New, username, password, client, deadline)
+}
+
+// SaslAuthScramSha512 performs SCRAM-SHA512 SASL authentication against an AuthClient.
+func SaslAuthScramSha512(username, password string, client AuthClient, deadline time.Time) error {
+	return saslAuthScram([]byte("SCRAM-SHA512"), sha512.New, username, password, client, deadline)
+}
+
 // SaslAuthBest performs SASL authentication against an AuthClient using the
 // best supported authentication algorithm available on both client and server.
 func SaslAuthBest(username, password string, client AuthClient, deadline time.Time) error {
@@ -62,34 +115,32 @@ func SaslAuthBest(username, password string, client AuthClient, deadline time.Ti
 			}
 		*/
 
-		// TODO: Support SCRAM-SHA Methods
+		if bestPriority <= 3 && method == "SCRAM-SHA1" {
+			bestPriority = 3
+			bestMethod = method
+		}
 
-		/*
-			if bestPriority <= 3 && method == "SCRAM-SHA1" {
-				bestPriority = 3
-				bestMethod = method
-			}
-		*/
+		if bestPriority <= 4 && method == "SCRAM-SHA256" {
+			bestPriority = 4
+			bestMethod = method
+		}
 
-		/*
-			if bestPriority <= 4 && method == "SCRAM-SHA256" {
-				bestPriority = 4
-				bestMethod = method
-			}
-		*/
-
-		/*
-			if bestPriority <= 5 && method == "SCRAM-SHA512" {
-				bestPriority = 5
-				bestMethod = method
-			}
-		*/
+		if bestPriority <= 5 && method == "SCRAM-SHA512" {
+			bestPriority = 5
+			bestMethod = method
+		}
 	}
 
 	logDebugf("Selected `%s` for SASL auth", bestMethod)
 
 	if bestMethod == "PLAIN" {
 		return SaslAuthPlain(username, password, client, deadline)
+	} else if bestMethod == "SCRAM-SHA1" {
+		return SaslAuthScramSha1(username, password, client, deadline)
+	} else if bestMethod == "SCRAM-SHA256" {
+		return SaslAuthScramSha256(username, password, client, deadline)
+	} else if bestMethod == "SCRAM-SHA512" {
+		return SaslAuthScramSha512(username, password, client, deadline)
 	}
 
 	return ErrNoAuthMethod
