3a4d166f73f9e26d98db9e76c0f01342f7eec7bc
Brett Lawson <brett19@gmail.com>
2017-03-17 15:42:36 -0700

    Improve pipeline management logging.
    Change-Id: Iae08c576ea413adc8dfdfc65cac8372556a14597
    Reviewed-on: http://review.couchbase.org/75560
    Reviewed-by: Mark Nunberg <mark.nunberg@couchbase.com>
    Tested-by: Brett Lawson <brett19@gmail.com>

--- memdpipeline.go
+++ memdpipeline.go
@@ -189,11 +189,14 @@ func (pipecli *memdPipelineClient) ioLoop(client *memdClient) {
 
 		<-client.CloseNotify()
 
+		logDebugf("Pipeline `%s/%p` client died", address, pipecli)
+
 		pipecli.lock.Lock()
 		pipecli.clientDead = true
 		consumer := pipecli.consumer
 		pipecli.lock.Unlock()
 
+		logDebugf("Pipeline `%s/%p` closing consumer %p", address, pipecli, consumer)
 		consumer.Close()
 
 		killSig <- struct{}{}
@@ -204,9 +207,13 @@ func (pipecli *memdPipelineClient) ioLoop(client *memdClient) {
 	var localConsumer *memdOpConsumer
 	for {
 		if localConsumer == nil {
+			logDebugf("Pipeline `%s/%p` fetching new consumer", address, pipecli)
+
 			pipecli.lock.Lock()
 
 			if pipecli.parent == nil {
+				logDebugf("Pipeline `%s/%p` found no parent", address, pipecli)
+
 				// This pipelineClient has been shut down
 				pipecli.lock.Unlock()
 				break
@@ -254,26 +261,34 @@ func (pipecli *memdPipelineClient) ioLoop(client *memdClient) {
 
 func (pipecli *memdPipelineClient) Run() {
 	for {
+		logDebugf("Pipeline Client `%p` preparing for new client loop", pipecli)
+
 		pipecli.lock.Lock()
 		pipeline := pipecli.parent
 		pipecli.clientDead = false
 		pipecli.lock.Unlock()
 
+		logDebugf("Pipeline Client `%p` is on parent: %p", pipecli, pipeline)
+
 		if pipeline == nil {
 			break
 		}
 
+		logDebugf("Pipeline Client `%p` retrieving new client connection", pipecli)
 		client, err := pipeline.getClientFn()
 		if err != nil {
 			continue
 		}
 
 		// Runs until the connection has died (for whatever reason)
+		logDebugf("Pipeline Client `%p` starting new client loop", pipecli)
 		pipecli.ioLoop(client)
 	}
 }
 
 func (pipecli *memdPipelineClient) Close() error {
+	logDebugf("Pipeline Client `%p` received close request", pipecli)
+
 	pipecli.lock.Lock()
 	pipecli.parent = nil
 	consumer := pipecli.consumer
