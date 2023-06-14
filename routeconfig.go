8673b63d7a4c71ef5d41b1636dd6442ba35be032
Charles Dixon <chvckd@gmail.com>
2023-06-14 10:51:37 +0100

    GOCBC-1426: Improve logging when configs are rejected
    Motivation
    ----------
    For improved debuggability we should include things like revid
    when logging a rejected config. We should also do it at info.
    
    Changes
    -------
    Added revid/revepoch to relevant logging config rejections.
    Change config rejection logging to info level.
    
    Change-Id: Ia01e03d720394768930bad479ea3e0b236e0007a
    Reviewed-on: https://review.couchbase.org/c/gocbcore/+/192600
    Reviewed-by: Brett Lawson <brett19@gmail.com>
    Tested-by: Charles Dixon <chvckd@gmail.com>

--- routeconfig.go
+++ routeconfig.go
@@ -143,16 +143,16 @@ func (config *routeConfig) ContainsBucketCapability(needleCap string) bool {
 
 func (config *routeConfig) IsNewerThan(oldCfg *routeConfig) bool {
 	if config.revEpoch < oldCfg.revEpoch {
-		logDebugf("Ignoring new configuration as it has an older revision epoch")
+		logInfof("Ignoring new configuration as it has an older revision epoch. Old: %d, new: %d", oldCfg.revEpoch, config.revEpoch)
 		return false
 	} else if config.revEpoch == oldCfg.revEpoch {
 		if config.revID == 0 {
-			logDebugf("Unversioned configuration data, switching.")
+			logInfof("Unversioned configuration data, switching.")
 		} else if config.revID == oldCfg.revID {
-			logDebugf("Ignoring configuration with identical revision number")
+			logInfof("Ignoring configuration with identical revision number - %d", config.revID)
 			return false
 		} else if config.revID < oldCfg.revID {
-			logDebugf("Ignoring new configuration as it has an older revision id")
+			logInfof("Ignoring new configuration as it has an older revision id. Old: %d, new: %d", oldCfg.revID, config.revID)
 			return false
 		}
 	}
